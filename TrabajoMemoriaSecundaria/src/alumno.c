5128093daf35836a3d039b6bc056eac3680c11be
alfasec <alfonso.cregcalv@gmail.com>
2026-05-29 17:49:55 +0200

    Revert "Merge branch 'main' of https://github.com/ALF4SEC/Estructuras-de-Datos-y-Algoritmo-II"
    This reverts commit af3240bfec52ac7aa999de311a497bde87b79431, reversing
    changes made to 67b8adc4a7b549ed978c1859dc8f14f76b187f13.

--- TrabajoMemoriaSecundaria/src/alumno.c
+++ TrabajoMemoriaSecundaria/src/alumno.c
@@ -3,16 +3,11 @@
 #include "../include/alumno.h"
 #include "../include/dispersion.h"
 
-<<<<<<< HEAD
 int funcionHash(tAlumno *reg, int nCubos){
     return atoi(reg->dni)%nCubos;
-=======
-int funcionHash(tAlumno *reg, int nCubos) {
-    return atoi(reg->dni) % nCubos;
->>>>>>> 26917ba03998ba32cb53411e8e8a03009aeb91ae
 }
 
-void mostrarReg(tAlumno *reg) {
+void mostrarReg(tAlumno *reg){
     fprintf(stdout, "DNI: %s\n", reg->dni);
     fprintf(stdout, "Num. Expediente: %d\n", reg->expediente);
     fprintf(stdout, "Nombre: %s\n", reg->nombre);
@@ -21,28 +16,24 @@ void mostrarReg(tAlumno *reg) {
     fprintf(stdout, "Provincia: %s\n", reg->provincia);
 }
 
-int cmpClave(tAlumno *reg1, tAlumno *reg2) {
-    return (!strcmp(reg1->dni, reg2->dni));
+int cmpClave(tAlumno *reg1, tAlumno *reg2){
+    return (!strcmp(reg1->dni, reg2->dni)); 
 }
 
-int buscar(char *fichero, char *dni) {
+int buscar(char *fichero, char *dni){
     FILE *ficheroEntrada;
     tPosicion pos;
     tipoReg reg;
     int resultado;
 
-    if ((ficheroEntrada = fopen(fichero, "rb")) == NULL) {
-        return -2;
+    if((ficheroEntrada=fopen(fichero, "rb"))==NULL){
+        fprintf(stderr, "Error: No se ha podido abrir de manera correcta el del fichero %s\n", fichero);
+        return -1;
     }
 
     strcpy(reg.dni, dni);
-<<<<<<< HEAD
 
     if(resultado=(busquedaHash(ficheroEntrada, &reg, &pos))==0){
-=======
-    
-    if ((resultado = busquedaHash(ficheroEntrada, &reg, &pos)) == 0) {
->>>>>>> 26917ba03998ba32cb53411e8e8a03009aeb91ae
         mostrarReg(&reg);
     }
 
@@ -50,47 +41,57 @@ int buscar(char *fichero, char *dni) {
     return resultado;
 }
 
-int modificar(char *fichero, char *dni, char *provincia) {
+int modificar(char *fichero, char *dni, char *provincia){
     FILE *ficheroEntrada;
     tPosicion pos;
     tipoReg reg;
     int resultado;
 
-    if ((ficheroEntrada = fopen(fichero, "rb+")) == NULL) {
-        return -2;
+    if((ficheroEntrada=fopen(fichero, "rb+"))==NULL){
+        fprintf(stderr, "Error: No se ha podido abrir de manera correcta el del fichero %s\n", fichero);
+        return -1;
     }
 
     strcpy(reg.dni, dni);
-<<<<<<< HEAD
     strcpy(reg.provincia, provincia);
-=======
->>>>>>> 26917ba03998ba32cb53411e8e8a03009aeb91ae
 
-    if ((resultado = busquedaHash(ficheroEntrada, &reg, &pos)) == 0) {
-        strcpy(reg.provincia, provincia);
-        resultado = modificarReg(ficheroEntrada, &reg, &pos);
+    if(resultado=(modificarReg(ficheroEntrada, &reg, &pos))==0){
+        mostrarReg(&reg);
     }
 
     fclose(ficheroEntrada);
     return resultado;
 }
 
-void pedirDatos(tAlumno *reg) {
+void pedirDatos(tAlumno *reg){
+    //Lectura del DNI
     fprintf(stdout, "Introduzca el DNI del alumno: ");
     scanf("%8s", reg->dni);
+    puts("");
 
-    fprintf(stdout, "Introduzca el numero de expediente del alumno: ");
+    //Lectura del numero de expediente
+    fprintf(stdout, "Introduzca el numero de experdiente del alumno: ");
     scanf("%d", &reg->expediente);
+    puts("");
 
+    //Lectura del nombre
     fprintf(stdout, "Introduzca el nombre del alumno: ");
-    scanf("%18s", reg->nombre); 
+    scanf("%19s", reg->nombre);
+    puts("");
 
+    //Lectura del apellido1
     fprintf(stdout, "Introduzca el primer apellido del alumno: ");
     scanf("%18s", reg->ape1);
+    puts("");
 
+    //Lectura del apellido2
     fprintf(stdout, "Introduzca el segundo apellido del alumno: ");
     scanf("%18s", reg->ape2);
+    puts("");
 
+    //Lectura de la provincia
     fprintf(stdout, "Introduzca la provincia del alumno: ");
     scanf("%10s", reg->provincia);
+    puts("");
+
 }
--- TrabajoMemoriaSecundaria/src/asignatura.c
+++ TrabajoMemoriaSecundaria/src/asignatura.c
@@ -3,11 +3,11 @@
 #include "../include/asignatura.h"
 #include "../include/dispersion.h"
 
-int funcionHash(tAsignatura *reg, int nCubos) {
-    return reg->codigo % nCubos;
+int funcionHash(tAsignatura *reg, int nCubos){
+    return (atoi(reg->codigo)%nCubo)
 }
 
-void mostrarReg(tAsignatura *reg) {
+void mostrarReg(tAsignatura *reg){
     fprintf(stdout, "Codigo de Asig.: %d\n", reg->codigo);
     fprintf(stdout, "Nombre de Asig.: %s\n", reg->nombre);
     fprintf(stdout, "Curso: %c\n", reg->curso);
@@ -15,27 +15,27 @@ void mostrarReg(tAsignatura *reg) {
     fprintf(stdout, "Creditos de Practicas: %f\n", reg->creditosP);
     fprintf(stdout, "Tipo de Asig.: %c\n", reg->tipo);
     fprintf(stdout, "Cuatrimestre: %c\n", reg->cuatrimestre);
-    fprintf(stdout, "Num. grupos de Teoria: %d\n", reg->numGrT);
-    fprintf(stdout, "Num. grupos de Practicas: %d\n", reg->numGrP);
+    fprintf(stdout, "Num. creditos de Teoria: %d\n", reg->numGrT);
+    fprintf(stdout, "Num. creditos de Practicas: %d\n", reg->numGrP);
 }
 
-int cmpClave(tAsignatura *reg1, tAsignatura *reg2) {
-    return (reg1->codigo == reg2->codigo);
+int cmpClave(tAsignatura *reg1, tAsignatura *reg2){
+    return(!(strcmp(reg1->codigo, reg2->codigo)))
 }
 
-int buscar(char *fichero, int codigo) {
-    FILE *ficheroEntrada;
+int buscar(char *fichero, int codigo){
+    FILE *fichEntrada;
     tipoReg reg;
     tPosicion pos;
-    int resultado;
 
-    if ((ficheroEntrada = fopen(fichero, "rb")) == NULL) {
-        return -2;
+    if((ficheroEntrada=fopen(fichero, "rb"))==NULL){
+        fprintf(stderr, "Error: No se ha podido abrir de manera correcta el del fichero %s\n", fichero);
+        return -1;
     }
 
-    reg.codigo = codigo;
+    reg->codigo=codigo;
 
-    if ((resultado = busquedaHash(ficheroEntrada, &reg, &pos)) == 0) {
+    if(resultado=(busquedaHash(ficheroEntrada, &reg, &pos))==0){
         mostrarReg(&reg);
     }
 
@@ -43,53 +43,73 @@ int buscar(char *fichero, int codigo) {
     return resultado;
 }
 
-int modificar(char *fichero, int codigo, float creditosT, float creditosP) {
+int modificar(char *fichero, int codigo, float creditosT, float creditosP){
     FILE *ficheroEntrada;
     tPosicion pos;
     tipoReg reg;
     int resultado;
 
-    if ((ficheroEntrada = fopen(fichero, "rb+")) == NULL) {
-        return -2;
+    if((ficheroEntrada=fopen(fichero, "rb+"))==NULL){
+        fprintf(stderr, "Error: No se ha podido abrir de manera correcta el del fichero %s\n", fichero);
+        return -1;
     }
 
-    reg.codigo = codigo;
+    reg->codigo=codigo;
+    reg->creditosT=creditosT;
+    reg->creditosP=creditosP;
 
-    if ((resultado = busquedaHash(ficheroEntrada, &reg, &pos)) == 0) {
-        reg.creditosT = creditosT;
-        reg.creditosP = creditosP;
-        resultado = modificarReg(ficheroEntrada, &reg, &pos);
+    if(resultado=(modificarReg(ficheroEntrada, &reg, &pos))==0){
+        mostrarReg(&reg);
     }
 
     fclose(ficheroEntrada);
     return resultado;
 }
 
-void pedirDatos(tAsignatura *reg) {
+void pedirDatos(tAsignatura *reg){
+    //Lectura del codigo
     fprintf(stdout, "Introduzca el codigo de la asig.: ");
     scanf("%d", &reg->codigo);
+    puts("");
 
+    //Lectura del nombre
     fprintf(stdout, "Introduzca el nombre de la asig.: ");
     scanf("%59s", reg->nombre);
+    puts("");
 
+    //Lectura del curso
     fprintf(stdout, "Introduzca el curso de la asig.: ");
-    scanf(" %c", &reg->curso);
+    scanf("%c", %reg->curso);
+    puts("");
 
-    fprintf(stdout, "Introduzca los creditos de teoria: ");
+    //Lectura del creditosT
+    fprintf(stdout, "Introduzca los creditos de la parte de teoria: ");
     scanf("%f", &reg->creditosT);
+    puts("");
 
-    fprintf(stdout, "Introduzca los creditos de practicas: ");
+    //Lectura del creditosP
+    fprintf(stdout, "Introduzca los creditos de la parte de practicas: ");
     scanf("%f", &reg->creditosP);
+    puts("");
 
+    //Lectura de la tipo
     fprintf(stdout, "Introduzca el tipo de la asig.: ");
-    scanf(" %c", &reg->tipo);
+    scanf("%c", reg->tipo);
+    puts("");
 
+    //Lectura de la cuatrimestre
     fprintf(stdout, "Introduzca el cuatrimestre de la asig.: ");
-    scanf(" %c", &reg->cuatrimestre);
+    scanf("%c", reg->cuatrimestre);
+    puts("");
 
+    //Lectura de la numGrT
     fprintf(stdout, "Introduzca el grupo de teoria: ");
     scanf("%d", &reg->numGrT);
+    puts("");
 
+    //Lectura de la numGrP
     fprintf(stdout, "Introduzca el grupo de practicas: ");
     scanf("%d", &reg->numGrP);
+    puts("");
 }
+
